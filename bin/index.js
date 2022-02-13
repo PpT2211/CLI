c39df6955da5dec1359868e2886ef9bb3e3b00e6
Pavan Teja <ptksed032@gmail.com>
2022-02-13 23:41:17 +0530

    added covid scraper and a smol cript

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,8 @@
 import { createRequire } from "module"
 import { franc } from "franc"
 import utils from "./utils.js"
+import makeProject from "../custom_modules/makeProject.js"
+import scraper from "../custom_modules/scraper.js"
 
 const require = createRequire(import.meta.url)
 const yargs = require("yargs")
@@ -26,15 +28,30 @@ const usage = "\nUsage: test project";
 const options = yargs
     .usage(usage)
     .option("l", { alias: "languages", describe: "List all supported languages.", type: "boolean", demandOption: false })
+    .option("m", { alias: "make", describe: "Make a project folder. Includes an html file, a css file and a js file", type: "boolean", demandOption: false })
+    .option("c", { alias: "covid", describe: "Latest covid status. You can get no.of cases in a specific state by entering the state name at the end", type: "boolean", demandOption: false })
     .help(true)
     .argv;
 
-if (yargs.argv["_"][0] == null) {
-    utils.showOptions(usage)
-}
+
 
 if (yargs.argv.l == true || yargs.argv.languages == true) {
     utils.showAll();
+}
+else if (yargs.argv.m == true || yargs.argv.make == true) {
+    makeProject(yargs.argv._);
+}
+else if (yargs.argv.c == true || yargs.argv.covid == true) {
+    if (yargs.argv["_"][0] == null) {
+        scraper.allInfo()
+    } else if (yargs.argv._) {
+        for (let i of yargs.argv._) {
+            scraper.stateWise(i)
+        }
+    }
+}
+else if (yargs.argv["_"][0] == null) {
+    utils.showOptions(usage)
 }
 //TESTS AND CHECKS
 
--- bin/utils.js
+++ bin/utils.js
@@ -13,6 +13,8 @@ const showOptions = (usage) => {
     console.log("Options:")
     console.log('\t--version\t      ' + 'Show version number.' + '\t\t' + '[boolean]\r')
     console.log('    -l, --languages\t' + '      ' + 'List all languages.' + '\t\t' + '[boolean]\r')
+    console.log('    -m, --make\t' + '      ' + 'Make a project folder. Includes an html file, a css file and a js file.' + '\t\t' + '[boolean]\r')
+    console.log('    -c, --covid\t' + '      ' + 'Latest covid status. You can get no.of cases in a specific state by entering the state name at the end.' + '\t\t' + '[boolean]\r')
     console.log('\t--help\t\t      ' + 'Show help.' + '\t\t\t' + '[boolean]\n')
 }
 
